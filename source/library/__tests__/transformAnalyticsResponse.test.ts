64170a3aa1c481561798f57497a78e81cc83d76e
Andrew Leedham <AndrewLeedham@Outlook.com>
2019-11-21 16:29:14 +0000

    test: add getBrowserVersion tests

--- source/library/__tests__/transformAnalyticsResponse.test.ts
+++ source/library/__tests__/transformAnalyticsResponse.test.ts
@@ -1,8 +1,9 @@
 import {
   /*transfromAnalyticsResponse,*/ findVersion,
-  // getBrowserVersion,
+  getBrowserVersion,
   // getLatestVersion,
 } from '../transformAnalyticsResponse';
+import getBaseStats from '../getBaseStats';
 
 describe('findVersion', () => {
   test('major', () => {
@@ -38,7 +39,94 @@ describe('findVersion', () => {
   });
 });
 
-describe('getBrowserVersion', () => {});
+describe.only('getBrowserVersion', () => {
+  // More specific version matching is done in findVersion, this is just to test the regex matches all 3 levels.
+  test('major version', () => {
+    expect(
+      getBrowserVersion('Google Chrome 70', { chrome: { '70': 0 } })
+    ).toEqual({
+      browser: 'chrome',
+      version: '70',
+    });
+  });
+
+  test('minor version', () => {
+    expect(
+      getBrowserVersion('Google Chrome 70.8', { chrome: { '70': 0 } })
+    ).toEqual({
+      browser: 'chrome',
+      version: '70',
+    });
+  });
+
+  test('patch version', () => {
+    expect(
+      getBrowserVersion('Google Chrome 70.1.54123', { chrome: { '70': 0 } })
+    ).toEqual({
+      browser: 'chrome',
+      version: '70',
+    });
+  });
+
+  test('unrecognized browser name', () => {
+    expect(
+      getBrowserVersion('Invalid 70', { chrome: { '70': 0 } })
+    ).toBeUndefined();
+  });
+
+  test('(unknown version) chooses latest', () => {
+    expect(
+      getBrowserVersion('Opera (unknown version)', {
+        opera: { '1': 1, '3': 3, '2': 2 },
+      })
+    ).toEqual({
+      browser: 'opera',
+      version: '3',
+    });
+  });
+
+  test('no version chooses latest', () => {
+    expect(
+      getBrowserVersion('Safari', { safari: { '1': 1, '3': 3, '2': 2 } })
+    ).toEqual({
+      browser: 'safari',
+      version: '3',
+    });
+  });
+
+  test('safari 0.8.2 chooses latest', () => {
+    expect(
+      getBrowserVersion('Safari 0.8.2', { safari: { '1': 1, '3': 3, '2': 2 } })
+    ).toEqual({
+      browser: 'safari',
+      version: '3',
+    });
+  });
+
+  test('whitespace trimmed', () => {
+    expect(
+      getBrowserVersion(' Google Chrome  70    ', {
+        chrome: { '70': 0, '71': 1 },
+      })
+    ).toEqual({
+      browser: 'chrome',
+      version: '70',
+    });
+  });
+
+  test('whitespace trimmed when no version', () => {
+    expect(
+      getBrowserVersion(' Google Chrome    ', { chrome: { '70': 0 } })
+    ).toEqual({
+      browser: 'chrome',
+      version: '70',
+    });
+  });
+
+  test('only a version', () => {
+    expect(getBrowserVersion('1.2.3', { chrome: { '70': 0 } })).toBeUndefined();
+  });
+});
 
 describe('getLatestVersion', () => {});
 
